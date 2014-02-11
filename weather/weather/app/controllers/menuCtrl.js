602a5e6e4da09c318bd7ba1a31658c86ed54e865
netmonster <netmonster_01@yahoo.com>
2014-02-11 10:37:43 -0600

    added missing files

---
+++ weather/weather/app/controllers/menuCtrl.js
@@ -0,0 +1,9 @@
+﻿angular.module('hwApp').controller('menuCtrl',
+ function ($scope, $log, $location, Weather, UserService, localStorageService) {
+     $scope.currentLocation = function ()
+     {
+         UserService.user.location = 'autoip';
+         toastr.success('updated Location: Using your Current Location:');
+         UserService.save();
+     };
+ })
